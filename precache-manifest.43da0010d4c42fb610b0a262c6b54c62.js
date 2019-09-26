0f8458a747846cd622ab1ac418caf20ba6034248
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-25 21:20:45 -0400

    updated

---
+++ precache-manifest.43da0010d4c42fb610b0a262c6b54c62.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72870082e72ee412a2ce6511cd3f372",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bb5186c1b63cacd47a8b",
+    "url": "/portfolio/static/css/main.6f8092cf.chunk.css"
+  },
+  {
+    "revision": "19f5134b6eec4023a9ff",
+    "url": "/portfolio/static/js/2.0c5da760.chunk.js"
+  },
+  {
+    "revision": "bb5186c1b63cacd47a8b",
+    "url": "/portfolio/static/js/main.aed3d6b7.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "36a4b2595f70321b149d2c62200e6995",
+    "url": "/portfolio/static/media/Chello-photo.36a4b259.png"
+  },
+  {
+    "revision": "cb7b2f777ec9dbdb93caad46ce442333",
+    "url": "/portfolio/static/media/Furbook-photo.cb7b2f77.png"
+  },
+  {
+    "revision": "4c97c3f9cf9a4f9501afcc6e066ee6fb",
+    "url": "/portfolio/static/media/HalsteinResume2019.4c97c3f9.pdf"
+  },
+  {
+    "revision": "1bd4999487a07ef98baca0ea3d56a1c4",
+    "url": "/portfolio/static/media/home-background.1bd49994.jpeg"
+  },
+  {
+    "revision": "74954344eb7bd44feb308704d087fcd2",
+    "url": "/portfolio/static/media/me.74954344.JPG"
+  },
+  {
+    "revision": "b6c8bea51df8c696543db4b11ae13ab3",
+    "url": "/portfolio/static/media/myStocks-photo.b6c8bea5.png"
+  }
+]);
