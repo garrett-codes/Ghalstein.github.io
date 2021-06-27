3135a834eaab5f3ce5b0d4841e3668aa37f91ae9
Garrett Halstein <ghalstein97@gmail.com>
2021-06-27 00:14:18 -0400

    updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.53291d04090ac711dbc385bef2beff1e.js"
+  "/portfolio/precache-manifest.809665a198995b84c200af353beeae07.js"
 );
 
 self.addEventListener('message', (event) => {
